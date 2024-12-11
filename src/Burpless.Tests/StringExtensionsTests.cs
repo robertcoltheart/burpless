edd5a3a774a9bee04dd0e2e0dfbe4e2fce8068b4
Robert Coltheart <robert.coltheart@gmail.com>
2024-12-12 09:04:46 +1100

    Convert method names to sentences

---
+++ src/Burpless.Tests/StringExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Burpless.Tests;
+
+public class StringExtensionsTests
+{
+    [Theory]
+    [InlineData("MethodNameTurnedIntoSentence", "method name turned into sentence")]
+    [InlineData("SomethingWith1stNumbering", "something with 1st numbering")]
+    [InlineData("ANameStartingWithA", "a name starting with a")]
+    [InlineData("WhenABigNumber1234IsInTheMiddle", "when a big number 1234 is in the middle")]
+    [InlineData("When_a_method_is_underscored", "when a method is underscored")]
+    [InlineData("With__multiple__underscores____here", "with multiple underscores here")]
+    [InlineData("WhenIUseMyOwnName", "when I use my own name")]
+    [InlineData("WithANumberAtTheEnd100", "with a number at the end 100")]
+    [InlineData("WithTheUSGovernment", "with the US government")]
+    [InlineData("AnABCAgreement", "an ABC agreement")]
+    public void CanConvertMethodNameToSentence(string input, string expected)
+    {
+        Assert.Equal(expected, input.ToSentence());
+    }
+}
---
+++ src/Burpless/StringExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Burpless;
+
+internal static class StringExtensions
+{
+    private const string Word = @"\p{Lu}?\p{Ll}+";
+
+    private const string NumberFollowedByLetters = @"[0-9]+\p{Ll}*";
+
+    private const string Acronym = @"\p{Lu}+(?=\p{Lu}|[0-9]|\b)";
+
+    private const string OtherLetters = @"\p{Lo}+";
+
+    private static readonly Regex PascalCaseWordPartsRegex = new(
+        $"{Word}|{NumberFollowedByLetters}|{Acronym}|{OtherLetters}",
+        RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+
+    public static string ToSentence(this string value)
+    {
+        if (value.All(char.IsUpper))
+        {
+            return value;
+        }
+
+        var words = PascalCaseWordPartsRegex.Matches(value)
+            .OfType<Match>()
+            .Select(x =>
+            {
+                var isUppercase = x.Value.All(char.IsUpper);
+
+                var isPronoun = x.Value == "I";
+                var isAcronym = isUppercase && x.Value.Length > 1;
+
+                if (isPronoun || isAcronym)
+                {
+                    return x.Value;
+                }
+
+                return x.Value.ToLower();
+            });
+
+        return string.Join(" ", words);
+    }
+}
