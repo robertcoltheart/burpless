1a7926e557a018cf355ed38f30693897d950c6e8
Robert Coltheart <robert.coltheart@gmail.com>
2024-12-15 07:07:07 +1100

    Simple service provider for resolving contexts

---
+++ src/Burpless.Tests/Services/SimpleServiceProviderTests.cs
@@ -0,0 +1,60 @@
+using System.Text;
+using Burpless.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Burpless.Tests.Services;
+
+public class SimpleServiceProviderTests
+{
+    private readonly SimpleServiceProvider provider = new();
+
+    [Fact]
+    public void CanResolveClass()
+    {
+        var result = provider.GetService<Standalone>();
+
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void CanResolveClassAndDependencies()
+    {
+        var result = provider.GetService<WithDependency>();
+
+        Assert.NotNull(result);
+        Assert.NotNull(result.Standalone);
+    }
+
+    [Fact]
+    public void SameDependencyIsInjectedTwice()
+    {
+        var result = provider.GetService<WithChainedDependencies>();
+
+        Assert.NotNull(result);
+        Assert.NotNull(result.Dependency);
+        Assert.NotNull(result.Standalone);
+        Assert.Same(result.Standalone, result.Dependency.Standalone);
+    }
+
+    [Fact]
+    public void CannotResolveInterface()
+    {
+        var exception = Record.Exception(() => provider.GetService<IServiceProvider>());
+
+        Assert.NotNull(exception);
+    }
+
+    [Fact]
+    public void CannotResolveAbstract()
+    {
+        var exception = Record.Exception(() => provider.GetService<Encoding>());
+
+        Assert.NotNull(exception);
+    }
+
+    private record Standalone;
+
+    private record WithDependency(Standalone Standalone);
+
+    private record WithChainedDependencies(Standalone Standalone, WithDependency Dependency);
+}
--- src/Burpless/Services/SimpleServiceProvider.cs
+++ src/Burpless/Services/SimpleServiceProvider.cs
@@ -4,6 +4,57 @@ internal class SimpleServiceProvider : IServiceProvider
 {
     public object GetService(Type serviceType)
     {
-        throw new NotImplementedException();
+        return Resolve(serviceType, new Dictionary<Type, object>(), new Stack<Type>());
+    }
+
+    private object Resolve(Type type, Dictionary<Type, object> created, Stack<Type> activating)
+    {
+        if (created.TryGetValue(type, out var value))
+        {
+            return value;
+        }
+
+        ValidateResolution(type, activating);
+
+        activating.Push(type);
+
+        created[type] = value = Create(type, created, activating);
+
+        activating.Pop();
+
+        return value;
+    }
+
+    private object Create(Type type, Dictionary<Type, object> created, Stack<Type> activating)
+    {
+        var constructor = type
+            .GetConstructors()
+            .OrderByDescending(x => x.GetParameters().Length)
+            .FirstOrDefault();
+
+        if (constructor == null)
+        {
+            throw new InvalidOperationException($"Cannot resolve {type}, no public constructors found");
+        }
+
+        var parameters = constructor
+            .GetParameters()
+            .Select(x => Resolve(x.ParameterType, created, activating))
+            .ToArray();
+
+        return Activator.CreateInstance(type, parameters);
+    }
+
+    private void ValidateResolution(Type type, Stack<Type> activating)
+    {
+        if (activating.Contains(type))
+        {
+            throw new InvalidOperationException($"Cannot resolve {type} as it would result in a circular dependency");
+        }
+
+        if (type.IsInterface || type.IsAbstract)
+        {
+            throw new InvalidOperationException($"Type {type} cannot be constructed");
+        }
     }
 }
