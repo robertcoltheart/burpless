accb639402a3b5d9e4a873b189a20d01ee82d51d
Robert Coltheart <robert.coltheart@gmail.com>
2025-01-12 10:13:43 +1100

    Rename configuration class

--- src/Burpless/TestSettings.cs
+++ src/Burpless/TestSettings.cs
@@ -5,17 +5,17 @@ namespace Burpless;
 /// <summary>
 /// Provides a mechanism for global configuration to be set.
 /// </summary>
-public class Configuration
+public class TestSettings
 {
     internal IServiceProvider Services { get; private set; } = new SimpleServiceProvider();
 
-    internal static Configuration Instance { get; } = new();
+    internal static TestSettings Instance { get; } = new();
 
     /// <summary>
     /// Sets the global configuration values.
     /// </summary>
-    /// <param name="action">A delegate for configuring the <see cref="Configuration"/>.</param>
-    public static void Initialize(Action<Configuration> action)
+    /// <param name="action">A delegate for configuring the <see cref="TestSettings"/>.</param>
+    public static void Configure(Action<TestSettings> action)
     {
         action(Instance);
     }
@@ -25,7 +25,7 @@ public class Configuration
     /// </summary>
     /// <param name="provider">The service provider to use when resolving contexts and their injected services.</param>
     /// <returns>The configuration.</returns>
-    public Configuration UseServiceProvider(IServiceProvider provider)
+    public TestSettings UseServiceProvider(IServiceProvider provider)
     {
         Services = new HybridServiceProvider(provider);
 
