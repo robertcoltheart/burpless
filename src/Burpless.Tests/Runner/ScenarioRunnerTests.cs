637f5d53033a01e937597d315c810634e977c4f3
Robert Coltheart <robert.coltheart@gmail.com>
2025-01-13 10:47:48 +1100

    add tests

--- src/Burpless.Tests/Runner/ScenarioRunnerTests.cs
+++ src/Burpless.Tests/Runner/ScenarioRunnerTests.cs
@@ -9,12 +9,15 @@ public class ScenarioRunnerTests
 
     private readonly Context context = new();
 
+    private readonly AnotherContext anotherContext = new();
+
     private readonly FeatureContext featureContext = new();
 
     public ScenarioRunnerTests()
     {
         A.CallTo(() => services.GetService(typeof(Context))).Returns(context);
         A.CallTo(() => services.GetService(typeof(FeatureContext))).Returns(featureContext);
+        A.CallTo(() => services.GetService(typeof(AnotherContext))).Returns(anotherContext);
     }
 
     [Test]
@@ -102,6 +105,22 @@ public class ScenarioRunnerTests
         A.CallTo(() => services.GetService(typeof(FeatureContext))).MustNotHaveHappened();
     }
 
+    [Test]
+    public async Task RunsWithMultipleContextsAsSingletons()
+    {
+        var details = new ScenarioDetails();
+        details.Steps.Add(new ScenarioStep<Context>("When", StepType.When, c => c.Call()));
+        details.Steps.Add(new ScenarioStep<Context>("Then", StepType.Then, c => c.Call()));
+        details.Steps.Add(new ScenarioStep<AnotherContext>("Then also", StepType.Then, c => c.Call()));
+        details.Steps.Add(new ScenarioStep<AnotherContext>("But", StepType.Then, c => c.Call()));
+
+        var runner = new ScenarioRunner(services, details);
+        await runner.Execute();
+
+        await Assert.That(context.Called).IsEqualTo(2);
+        await Assert.That(anotherContext.Called).IsEqualTo(2);
+    }
+
     private class Context
     {
         public int Called;
@@ -115,4 +134,6 @@ public class ScenarioRunnerTests
     }
 
     private class FeatureContext : Context;
+
+    private class AnotherContext : Context;
 }
