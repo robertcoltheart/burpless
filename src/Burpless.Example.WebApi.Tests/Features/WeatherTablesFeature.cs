24ac1645b7093fbff501cb445f3fbb4d81be6a67
Robert Coltheart <robert.coltheart@gmail.com>
2025-01-14 12:04:06 +1100

    tests

--- src/Burpless.Example.WebApi.Tests/Features/WeatherTablesFeature.cs
+++ src/Burpless.Example.WebApi.Tests/Features/WeatherTablesFeature.cs
@@ -15,8 +15,8 @@ public class WeatherTablesFeature
             | 4            | Bracing  |
             """))
         .And(x => x.TheFollowingDataIsReturned(Table.From(
-            new Weather { TemperatureC = 3, Summary = "Freezing", Date = new DateOnly(2024, 12, 25), TemperatureF = 76 },
-            new Weather { TemperatureC = 4, Summary = "Bracing", Date = new DateOnly(2024, 12, 26), TemperatureF = 78 })))
+            new Weather { TemperatureC = 3, Summary = "Freezing", Date = new DateOnly(2024, 12, 25), TemperatureF = 37 },
+            new Weather { TemperatureC = 4, Summary = "Bracing", Date = new DateOnly(2024, 12, 26), TemperatureF = 39 })))
         .And(x => x.TheFollowingDataIsReturned(Table
             .WithColumns("TemperatureC", "Summary")
             .AddRow("3", "Freezing")
@@ -27,5 +27,5 @@ public class WeatherTablesFeature
         .When(x => x.TheWeatherForecastIsFetched())
         .Then(x => x.TheFollowingDataIsReturned(Table.Validate<Weather>(validator => validator
             .WithColumn(c => c.TemperatureC, c => c > 2)
-            .WithColumn(c => c.TemperatureF, c => c > 30))));
+            .WithColumn(c => c.TemperatureF, c => c > 35))));
 }
